22268461a355c31fb7364c93a57de46d799640eb
Cesar Gattano <cesar.gattano@gmail.com>
2025-04-29 16:07:41 +0200

    add function add_deck_to_board in boards.py

---
+++ boards.py
@@ -0,0 +1,32 @@
+import lists
+
+deck_test = {
+    "name": "deck test",
+    "card_list": list(),
+    "origin_board": "board test",
+    "description": "Example of a deck for test purpose."
+}
+
+board_test = {
+    "name": "board test",
+    "deck_list": list(),
+    "description": "Example of a board for test purpose."
+}
+
+board_list = [board_test]
+
+def add_deck_to_board(deck: dict, board_name: str) -> bool:
+    
+    # Add a deck to the input board
+    for board in board_list:
+        if (board["name"] == board_name):
+            board["deck_list"].append(deck)
+            return True
+
+
+def main():
+    print(add_deck_to_board(deck_test, "board test"))
+    print(board_test)
+
+if __name__ == "__main__":
+    main()
