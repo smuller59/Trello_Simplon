921e0c8fee900b0073b3251d44c892050ecd45ee
stephane <Vous@exemple.com>
2025-04-30 12:48:56 +0200

    modif faite

--- decks.py
+++ decks.py
@@ -1,11 +1,6 @@
 import boards as b
 import cards as c 
 
-deck_list = {"name" : "nom du deck",
-       "card_list" :[],
-       "description" : "description du deck",
-       "origin_board" : "board1" }
-
 def add_card_to_deck(card:dict, deck_name:str):
     #rechercher dans deck_list le deck qui a le nom deck_name
     for deck in deck_list:
@@ -15,23 +10,17 @@ def add_card_to_deck(card:dict, deck_name:str):
             return True
         else:
             return False
-
-template_deck = {"name" : "Trelloplon",
-                 "card_list":[],
-                "description":"bouchon_deck",
-                "origin_board" : "Origin_toto"
-                }
-                
-
-def create_deck(name: str, description:str, origin_board:str) -> dict:
+  
+def create_deck(name: dict, description:str, origin_board:str) -> dict:
+    template_deck=dict
     template_deck = {"name" : "Trelloplon",
                  "card_list":[],
                 "description":"bouchon_deck",
                 "origin_board" : "Origin_toto"
                 }
-    card1 = c.create_card ("card1", "description de la carte 1", "Trelloplon")
-    card2 = c.create_card ("card2", "description de la carte 2", "Trelloplon")
-    card3 = c.create_card ("card3", "description de la carte 2", "Trelloplon")
+    card1 = c.create_card("card1", "description de la carte 1", "Trelloplon")
+    card2 = c.create_card("card2", "description de la carte 2", "Trelloplon")
+    card3 = c.create_card("card3", "description de la carte 2", "Trelloplon")
     cards= [card1,card2,card3]
     template_deck["name"]= name
     template_deck["card_list"]= cards
