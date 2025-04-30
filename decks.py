0531a219e457f9885a5babda8a31f9a6ff223969
stephane <Vous@exemple.com>
2025-04-30 11:48:36 +0200

    ajout fonction création deck et bouchon deck nommé deck_template

--- decks.py
+++ decks.py
@@ -1,5 +1,5 @@
-import boards
-import cards
+import boards as b
+import cards as c 
 
 deck_list = {"name" : "nom du deck",
        "card_list" :[],
@@ -16,13 +16,34 @@ def add_card_to_deck(card:dict, deck_name:str):
         else:
             return False
 
-def create_deck():
-    """Fonction qui crée une liste et l'ajoute au boards"""
-    new_deck= [{deck_list}]
-    add_deck_board=new_deck.append({boards})
-    return add_deck_board
+template_deck = {"name" : "Trelloplon",
+                 "card_list":[],
+                "description":"bouchon_deck",
+                "origin_board" : "Origin_toto"
+                }
+                
 
+def create_deck(name: str, description:str, origin_board:str) -> dict:
+    template_deck = {"name" : "Trelloplon",
+                 "card_list":[],
+                "description":"bouchon_deck",
+                "origin_board" : "Origin_toto"
+                }
+    card1 = c.create_card ("card1", "description de la carte 1", "Trelloplon")
+    card2 = c.create_card ("card2", "description de la carte 2", "Trelloplon")
+    card3 = c.create_card ("card3", "description de la carte 2", "Trelloplon")
+    cards= [card1,card2,card3]
+    template_deck["name"]= name
+    template_deck["card_list"]= cards
+    template_deck["description"]= description
+    template_deck["origin_board"]= origin_board
+    return template_deck
 pass
+
+def request_card(name:str):
+    #demande une carte
+    demande_carte= get_create_card()
+    return (cards)
 
 def delete_deck():
     """Fonction qui supprime une liste"""
