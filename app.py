64898ad1645c05f272d17a2f7e040471fb33b4e2
olivier <olivier.legendre.pro@gmail.com>
2025-04-30 15:30:04 +0200

    suppression d'un board via trello

--- app.py
+++ app.py
@@ -2,11 +2,29 @@ import json
 import trello as t
 
 
+def display_trello(trello):
+    for board in trello:
+        for key_board, value_board in board.items():
+            if key_board == "deck_list":
+                # print(f"mon deck: {value_board}")
+                for deck in value_board:
+                    for key_deck, value_deck in deck.items():
+                        if key_deck == 'card_list':
+                            print(f"card list : {value_deck}")
+                        else:
+                            print(f"{key_deck}{value_deck}")
+            else:
+                print(f"{key_board}: {value_board}")
+        print("\n")
+            
+
 def main():
     trello = t.create_trello()
-    print(trello)
+    # display_trello(trello)
     t.delete_board("ma board numero 1", trello)
-    print(trello)
+    display_trello(trello)
+    
+    # print(trello)
     trello_js = json.dumps(trello)
     # print(trello_js)
 
